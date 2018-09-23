bc0771655e5bda442e57f199554e2e5627f492d8
EC2 Default User <ec2-user@ip-172-31-21-219.us-east-2.compute.internal>
2018-09-23 04:55:08 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,23 +1,32 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :order)
   end
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
-    @movies = Movie.all
+    @order = params[:order]
+    
+    # Check order param for any ordering required
+    if @order == "title"
+      @movies = Movie.order(:title)
+    elsif  @order == "date"
+      @movies = Movie.order(:release_date)
+    else
+      @movies = Movie.all
+    end
   end
 
   def new
     # default: render 'new' template
   end
-
+ 
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
